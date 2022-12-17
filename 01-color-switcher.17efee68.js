8059b27cc7408834e072b07cdf95aa561acfd7d2
ArtiTemkin <103365069+ArtiTemkin@users.noreply.github.com>
2022-12-17 13:13:15 +0000

    Deploying to gh-pages from @ ArtiTemkin/goit-js-hw-09@af475bb0211069ded9557ec7a8ee8f93ef484569 🚀

---
+++ 01-color-switcher.17efee68.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");document.querySelector("body");e.addEventListener("click",(function(){o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0})),t.addEventListener("click",(function(){clearInterval(o),e.disabled=!1}));let o=null;
+//# sourceMappingURL=01-color-switcher.17efee68.js.map
